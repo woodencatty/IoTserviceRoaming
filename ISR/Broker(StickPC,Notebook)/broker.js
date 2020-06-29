7fb93fc1b75422ed717d221ce95aa7b36d87db50
woodencatty <woodencatty@gmail.com>
2020-06-29 20:49:23 +0900

    basic session assigned

--- ISR/Broker(StickPC,Notebook)/broker.js
+++ ISR/Broker(StickPC,Notebook)/broker.js
@@ -1,55 +1,88 @@
 var mosca = require('mosca');
- 
 var mqtt = require('mqtt')
-
 var moment = require('moment');
 require('moment-timezone');
 
 var ascoltatore = {
-  //using ascoltatore
   type: 'mongo',
   url: 'mongodb://localhost:27017/mqtt',
   pubsubCollection: 'ascoltatori',
   mongo: {}
 };
- 
-var moment = require('moment');
-require('moment-timezone');
 
 var settings = {
   port: 1883,
   backend: ascoltatore
 };
- 
+
 var server = new mosca.Server(settings);
- 
-server.on('clientConnected', function(client) {
-    console.log('client connected', client.id);
-});
- 
-// fired when a message is received
-server.on('published', function(packet, client) {
-  moment.tz.setDefault("Asia/Seoul");
-  var timeNow = moment().format('YYYY-MM-DD HH:mm:ss.SSS');
-  console.log('Published at '+timeNow+ " Sent at "+ packet.payload.toString());
-});
- 
+
 server.on('ready', setup);
- 
-// fired when the mqtt server is ready
 function setup() {
-  console.log('Mosca server is up and running');
+  console.log('ISR Platform Broker on');
 }
 
+var Agent_to_Master = mqtt.connect('mqtt://127.0.0.1:1884')
+Agent_to_Master.on('connect', function () {
+  console.log('ISR Agent Server on');
+})
+
+// fired when a message is received
+server.on('published', function (packet, client) {
+  moment.tz.setDefault("Asia/Seoul");
+  var timeNow = moment().format('YYYY-MM-DD HH:mm:ss.SSS');
+  var topic_arr = packet.topic.split("/");
+  // if(topic_arr[3] == "subscribes"){
+  //   console.log('Published at '+timeNow+ "| topic :  "+packet.topic+" | message : "+ packet.payload);
+  // }
+});
+
+
 //ISR Agent-----------------------------------------------------------------------------
- 
-server.on('clientDisconnected', function(client) {
-  console.log('clientDisconnected', client.id);
 
-  var client = mqtt.connect('mqtt://127.0.0.1:1884')
 
-  client.on('connect', function () {
-      client.publish('/User/'+client.id, "leave");
-  })
-  
+server.on('subscribed', (topic, client) => {
+  var topic_arr = topic.split("/");
+  var user_id = topic_arr[0]
+
+  if (topic_arr[1] == "session") {
+    if (topic_arr[2] == "request") {
+      console.log(user_id + " requested " + topic)
+
+      Agent_to_Master.subscribe(user_id + '/session/request', function (err) {
+        console.log("Session Applied to Master (error :" + err + ")");
+      })
+    }
+  }
+})
+
+Agent_to_Master.on('message', function (topic, message) {
+  var topic_arr = topic.split("/");
+  var user_id = topic_arr[0]
+
+  if (topic_arr[1] == "session") {
+    if (topic_arr[2] == "request") {
+      console.log(user_id + " Assigned ");
+      Agent_to_Master.unsubscribe(user_id + '/session/request', ()=>{
+        var Agent_to_Client = mqtt.connect('mqtt://127.0.0.1:1883')
+        Agent_to_Client.on('connect', function () {
+          console.log('ISR Agent Client on');
+          Agent_to_Client.publish(user_id + '/session/request', message.toString())
+          Agent_to_Client.end();
+        })
+      })
+    }
+  }
+})
+
+
+server.on('clientDisconnected', function (client) {
+  console.log('clientDisconnected ', client.id);
+
+  //var client = mqtt.connect('mqtt://127.0.0.1:1884')
+
+  // client.on('connect', function () {
+  //     client.publish('/User/'+client.id, "leave");
+  // })
+
 });
--- ISR/MasterBroker(MainHouse)/masterbroker.js
+++ ISR/MasterBroker(MainHouse)/masterbroker.js
@@ -1,5 +1,10 @@
 var mosca = require('mosca');
- 
+
+var mqtt = require('mqtt')
+
+var moment = require('moment');
+require('moment-timezone');
+
 var ascoltatore = {
   //using ascoltatore
   type: 'mongo',
@@ -7,32 +12,71 @@ var ascoltatore = {
   pubsubCollection: 'ascoltatori',
   mongo: {}
 };
- 
-var moment = require('moment');
-require('moment-timezone');
 
 var settings = {
   port: 1884,
   backend: ascoltatore
 };
- 
+
 var server = new mosca.Server(settings);
- 
-server.on('clientConnected', function(client) {
-    console.log('client connected', client.id);
-});
- 
-// fired when a message is received
-server.on('published', function(packet, client) {
-  console.log("sent_log "+ packet.payload.toString());
-});
- 
 server.on('ready', setup);
- 
-// fired when the mqtt server is ready
+
 function setup() {
-  console.log('Mosca server is up and running');
+  console.log('ISR Platform Broker on');
 }
+
+// fired when a message is received
+server.on('published', function (packet, client) {
+  moment.tz.setDefault("Asia/Seoul");
+  var timeNow = moment().format('YYYY-MM-DD HH:mm:ss.SSS');
+  var topic_arr = packet.topic.split("/");
+  // if(topic_arr[3] == "subscribes"){
+  //   console.log('Published at '+timeNow+ "| topic :  "+packet.topic+" | message : "+ packet.payload);
+  // }
+});
 
 //ISR Manager-----------------------------------------------------------------------------
 
+
+server.on('subscribed', (topic, client) => {
+  var topic_arr = topic.split("/");
+  var user_id = topic_arr[0]
+
+  if (topic_arr[1] == "session") {
+    if (topic_arr[2] == "request") {
+
+      var ISR_Manager = mqtt.connect('mqtt://127.0.0.1:1884')
+      ISR_Manager.on('connect', function () {
+        console.log('ISR Manager on');
+      })
+      console.log(user_id + " requested " + topic)
+      ISR_Manager.publish(topic, '{"session" : "' + makeSessionId(6) + ', "status" : "granted"}')
+      console.log("session granted to " + user_id)
+
+      ISR_Manager.end();
+    }
+  }
+
+})
+
+server.on('clientDisconnected', function (client) {
+  console.log('clientDisconnected ', client.id);
+
+  //var client = mqtt.connect('mqtt://127.0.0.1:1884')
+
+  // client.on('connect', function () {
+  //     client.publish('/User/'+client.id, "leave");
+  // })
+
+});
+
+
+function makeSessionId(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
